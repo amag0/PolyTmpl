963c423009a5f3a19330dd76c04ebeaa0747e664
amag0 <30939985+amag0@users.noreply.github.com>
2018-11-14 20:20:33 +0100

    restart the project
    hello world like application

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  
+  // normal program termination
+  std::cout<<"press Enter to continue..."<<std::endl;
+  std::cin.ignore();
+  return 0x0; 
+}
